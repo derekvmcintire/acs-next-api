90a646435824e518da9adcc1a138ef94ee208ddf
Derek McIntire <derekbmcintire@gmail.com>
2024-10-24 14:24:12 -0400

    convert db.js to typescript

---
+++ pages/api/db.ts
@@ -0,0 +1,16 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env as NodeJS.ProcessEnv;
+
+const pool = new Pool({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: Number(DB_PORT), // Convert port to number
+});
+
+export default pool;
