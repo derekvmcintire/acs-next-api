e23dba38c7c2bfd58d980929518a480a58859527
Derek McIntire <derekbmcintire@gmail.com>
2024-10-24 15:37:21 -0400

    Create tblRider

---
+++ sql/create-tblRider.sql
@@ -0,0 +1,12 @@
+CREATE TABLE tblRider (
+  id SERIAL PRIMARY KEY,
+  firstName varchar(255),
+  lastName varchar(255),
+  dob varchar(255),
+  country varchar(255),
+  hometown varchar(255),
+  photo varchar(255),
+  strava varchar(255),
+  insta varchar(255),
+  about varchar(255)
+);
