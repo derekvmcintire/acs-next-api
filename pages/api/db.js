c207d22dd4b8256496d0321836ed6d67e1e261f5
Derek McIntire <derekbmcintire@gmail.com>
2024-10-24 14:01:40 -0400

    u wish

--- pages/api/db.js
+++ pages/api/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'derek',         // your database username
     host: 'localhost',      // your database host
     database: 'acs', // your database name
-    password: 'under1t1', // your database password
+    password: 'uwish', // your database password
     port: 5432,             // your database port (default is 5432)
 });
 
