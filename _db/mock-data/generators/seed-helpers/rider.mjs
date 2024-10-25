629d71ee93cc6c28be4aed0c91bac1facfb56e78
Derek McIntire <derekbmcintire@gmail.com>
2024-10-25 15:48:26 -0400

    All data seeded into the DB

--- _db/mock-data/generators/seed-helpers/rider.mjs
+++ _db/mock-data/generators/seed-helpers/rider.mjs
@@ -1,21 +1,44 @@
 import { buildMockRacerInfo } from '../rider/build-rider.mjs';
-
+import { generateRandomNumber } from '../helper-functions.mjs';
 
 export const createRiders = async (client) => {
+  const teams = await client.team.findMany();
+  const categories = await client.category.findMany();
+  
   for (let i=1; i < 100; i++) {
-      const data = buildMockRacerInfo();
-      await client.rider.create({
+    const riderTeamId = teams[generateRandomNumber(teams.length - 1)].id;
+    const riderCategoryId = categories[generateRandomNumber(categories.length - 1)].id;
+    const data = buildMockRacerInfo();
+
+    // insert rider
+    const rider = await client.rider.create({
+      data: {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          dob: data.dob,
+          country: data.country,
+          hometown: data.hometown,
+          photo: data.photo,
+          strava: data.strava,
+          insta: data.insta,
+          about: data.about,
+        },
+    });
+
+      // insert into joinRiderTeam
+      await client.joinRiderTeam.create({
         data: {
-            firstName: data.firstName,
-            lastName: data.lastName,
-            dob: data.dob,
-            country: data.country,
-            hometown: data.hometown,
-            photo: data.photo,
-            strava: data.strava,
-            insta: data.insta,
-            about: data.about,
-          },
-      });
+          rider: {connect: {id: rider.id}},
+          team: {connect: {id: riderTeamId}},
+        }
+      })
+
+      // insert into joinRiderCategory
+      await client.joinRiderCategory.create({
+        data: {
+          rider: {connect: {id: rider.id}},
+          category: {connect: {id: riderCategoryId}}
+        }
+      })
   }
 }
