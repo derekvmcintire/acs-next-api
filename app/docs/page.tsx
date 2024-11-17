0e7dc1c7860454528fd50ccb29e5131ead28eda5
Derek McIntire <derekbmcintire@gmail.com>
2024-11-17 10:06:38 -0500

    Swagger docs working with css

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -2,7 +2,8 @@
 
 import dynamic from "next/dynamic";
 import React from "react";
-import "./theme.css";
+import "swagger-ui-react/swagger-ui.css"; // Correct path for the CSS
+// import './theme.css';
 
 const SwaggerUI = dynamic(
   () => import("swagger-ui-react").then((mod) => mod.default),
